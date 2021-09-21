4cd259c3ee359b1da55320f775059b973f51503a
Imran Azam <imranazam3@gmail.com>
2021-09-21 21:21:11 +0100

    setup react hot loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { hot } from "react-hot-loader/root";
+
 import "./App.css";
 
 const App = () => (
-  <div className="App">
-    <h1>Hello World!</h1>
+  <div>
+    <h1 class="center">Hello React!</h1>
   </div>
 );
 
-export default App;
+export default hot(App);
