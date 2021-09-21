d2a93a6df907722d37f83e52989e81f7f26cb227
Imran Azam <imranazam3@gmail.com>
2021-09-21 22:53:32 +0100

    removed item from list

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,7 +4,7 @@ import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
 
-const TodoList = ({ todos = [{ text: "hello" }, { text: "there" }] }) => (
+const TodoList = ({ todos = [{ text: "hello" }] }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
